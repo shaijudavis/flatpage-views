8ba1fa532475462e35243f71f72a4ecf905c49c8
Shaiju Davis <shaiu.davis@vanillanetworks.com>
2013-05-29 14:27:58 +0530

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flatpage_views',
+    version='0.1.1',
+    description='New view files for default flatapge',
+    author='shaiju',
+    author_email='shaijudavis@gmail.com',
+    url='https://github.com/shaijudavis/flatpage-views.git',
+    license='MIT',
+    packages=find_packages(),
+)
